8e79b980b7b2df735811d4bd1c6b00bc95793a36
fictionalspace <hannah.fu@sjsu.edu>
2022-03-09 17:08:07 -0800

    Selfface 1.0

---
+++ selfface/ellipse1/ellipse1.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 600);
+}
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 60, 80);
+}
---
+++ selfface/ellipse1/p5js-temp-SelfFace6251656736538726233.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(600, 600);
+}
+function draw() {
+  background(204);
+ 
+  ellipse(132, 82, 200, 200);   // Gray circle
+
+}
